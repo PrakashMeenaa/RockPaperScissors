741b34c1cad277439eb334b09bd3f945a40429a0
PrakashMeenaa <meenaprak12@gmail.com>
2023-12-11 22:52:15 +0530

    Addlistener Addlistener

--- RockpaperScissor.js
+++ RockpaperScissor.js
@@ -4,7 +4,9 @@ let computerchoice='';
             let c='Computer won';
             let w='You won';
             let result='';
+            let id1;
             let randomnum;
+            let autoplayquery=document.querySelector('.autoPlay');
             let score=JSON.parse(localStorage.getItem('score'))|| {
                 win: 0,
                 losses:0,
@@ -92,3 +94,53 @@ let computerchoice='';
                 }
                 console.log(computerchoice);
             }
+            function generateRandomChoice(){
+                randomnum = Math.floor(Math.random()*3) +1;
+                  if(randomnum==1){
+                      userchoice='Rock';
+                  }
+                  else if( randomnum==2){
+                      userchoice='Paper';
+                  }
+                  else{
+                      userchoice='Scissor';
+                  }
+                  console.log(computerchoice);
+              }
+              function Play(){
+                if(autoplayquery.innerText==='Autoplay'){
+                    autoplayquery.innerHTML='Stop Game';
+                 id1=setInterval(function(){ generateRandomChoice(); playgame(userchoice);},1000);
+                   }
+                else{
+                    autoplayquery.innerHTML='Autoplay';
+                    clearInterval(id1);
+                 }
+              }
+              function confirmation(){
+                document.querySelector('.confirmation_message').innerHTML=`<p style="display: inline-block;">Are you sure you want to reset the score ?</p> <button class="confirmation_button_yes">YES</button> <button class="confirmation_button_no">NO</button>`;
+                document.querySelector('.confirmation_button_no').addEventListener('click',()=>{document.querySelector('.confirmation_message').innerHTML=''});
+                document.querySelector('.confirmation_button_yes').addEventListener('click',()=>{reset(); document.querySelector('.confirmation_message').innerHTML='';});
+                
+              }
+
+              document.querySelector('.js_rock_button').addEventListener('click',()=>{playgame('Rock')});
+              document.querySelector('.js_paper_button').addEventListener('click',()=>{playgame('Paper')});
+              document.querySelector('.js_scissor_button').addEventListener('click',()=>{playgame('Scissor')});
+              document.querySelector('.js_resetbutton').addEventListener('click',()=>{confirmation()});
+              document.querySelector('.js_autoplay').addEventListener('click',()=>{Play()});
+             
+
+              document.body.addEventListener('keydown',(event)=>{if(event.key==='r')
+                                                                   playgame('Rock')
+                                                                else if(event.key==='p')
+                                                                   playgame('paper')
+                                                                else if(event.key==='s')
+                                                                   playgame('Scissor') 
+                                                                else if(event.key==='a')
+                                                                    Play() 
+                                                                else if(event.key==='Backspace')
+                                                                     confirmation() 
+                                                                    });
+
+              
